486b08a8d961074df6db95b0458c20cc209b05c3
Shubham Agdari <61975040+shubham007x@users.noreply.github.com>
2025-07-18 03:46:48 +0530

    Time: 13 ms (76.29%) | Memory: 44.7 MB (55.13%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maxVowels(String s, int k) {
-      
+
         int count = 0;
         for (int i = 0; i < k; i++) {
             char x = s.charAt(i);
-            if (check(x)) {
+            if (isVowel(x)) {
                 count++;
             }
         }
-        int max =count;
+        int max = count;
         for (int i = k; i < s.length(); i++) {
 
-            if (check(s.charAt(i - k))) {
+            if (isVowel(s.charAt(i - k))) {
                 count--;
             }
-            if (check(s.charAt(i))) {
+            if (isVowel(s.charAt(i))) {
                 count++;
             }
             max = Math.max(count, max);
@@ -26,10 +26,7 @@ class Solution {
 
     }
 
-    public static Boolean check(char x) {
-        if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
-            return true;
-        } else
-            return false;
+    private static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
 }
