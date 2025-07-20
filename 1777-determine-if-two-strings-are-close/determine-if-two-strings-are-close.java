3f3ff6274969e5595b190fd1383eee34ef105972
Shubham Agdari <61975040+shubham007x@users.noreply.github.com>
2025-07-20 18:55:29 +0530

    Time: 41 ms (46.71%) | Memory: 45.7 MB (67.90%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,34 +1,27 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
-
-        for (int i = 0; i < word1.length(); i++) {
-            char c = word1.charAt(i);
-            if (map1.containsKey(c)) {
-                int freq = map1.get(c);
-                map1.put(c, freq + 1);
-            } else {
-                map1.put(c, 1);
-            }
-        }
-        for (int i = 0; i < word2.length(); i++) {
-            char c = word2.charAt(i);
-            if (map2.containsKey(c)) {
-                int freq = map2.get(c);
-                map2.put(c, freq + 1);
-            } else {
-                map2.put(c, 1);
-            }
-        }
-        if (!map1.keySet().equals(map2.keySet())) {
+        if (word1.length() != word2.length()) {
             return false;
         }
-        List<Integer> freq1 = new ArrayList<>(map1.values());
-        List<Integer> freq2 = new ArrayList<>(map2.values());
-        Collections.sort(freq1);
-        Collections.sort(freq2);
-        return freq1.equals(freq2);
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        HashSet<Character> set1 = new HashSet<>();
+        HashSet<Character> set2 = new HashSet<>();
+        for (char c : word1.toCharArray()) {
+            arr1[c - 'a']++;
+            set1.add(c);
+        }
+        for (char c : word2.toCharArray()) {
+            arr2[c - 'a']++;
+            set2.add(c);
+        }
+
+        if (!set1.equals(set2)) {
+            return false;
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return (Arrays.equals(arr1, arr2));
 
     }
 }
