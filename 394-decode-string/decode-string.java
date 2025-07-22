57af4a427867c40008a0b7570fbdb055d9b5f707
Shubham Agdari <61975040+shubham007x@users.noreply.github.com>
2025-07-23 01:59:51 +0530

    Time: 5 ms (11.57%) | Memory: 42 MB (19.63%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String decodeString(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c != ']') {
+                st.push(c);
+            } else {
+                String str = "";
+                while (st.peek() != '[') {
+                    str += st.pop();
+                }
+                st.pop();
+                StringBuilder kStr = new StringBuilder();
+                while (!st.isEmpty() && Character.isDigit(st.peek())) {
+                    kStr.insert(0, st.pop());
+                }
+                int k = Integer.parseInt(kStr.toString());
+                String str1 = str.repeat(k);
+                System.out.println(k);
+
+                int i = str1.length() - 1;
+                while (i >= 0) {
+                    st.push(str1.charAt(i));
+                    i--;
+                }
+
+            }
+
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
